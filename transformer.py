17feafb22a787814e7b57fae5a05c4fd91aea8a4
irene <i.bini3@studenti.unipi.it>
2025-03-27 10:27:48 +0100

    minimal correction

--- transformer.py
+++ transformer.py
@@ -1,6 +1,5 @@
 import logging
 import argparse
-
 import ROOT
 import torch.nn as nn # questo contiene la classe transformer di pytorch
 import torch.optim as optimizer # sicuramente ci servirà
@@ -16,20 +15,19 @@ andrà fatta una funzione che PREPARA i dati che abbiamo simulato. per essere us
 
     un esempio è: 
     particles_status_23 = torch.tensor([
-        [1.0, 2.0, 3.0, 100.0, 0.5, 1],   # Particella 1, evento 1
-        [4.0, 5.0, 6.0, 200.0, 0.5, 2],   # Particella 2, evento 1
-        [7.0, 8.0, 9.0, 300.0, 0.5, 3],   # Particella 3, evento 1
-        [2.0, 3.0, 4.0, 150.0, 0.6, 4],   # Particella 1, evento 2
-        [5.0, 6.0, 7.0, 250.0, 0.6, 5],   # Particella 2, evento 2
-        [8.0, 9.0, 10.0, 350.0, 0.6, 6],  # Particella 3, evento 2
+        [
+            [1.0, 2.0, 3.0, 100.0, 0.5, 1],   # Particella 1, evento 1
+            [4.0, 5.0, 6.0, 200.0, 0.5, 2],   # Particella 2, evento 1
+            [7.0, 8.0, 9.0, 300.0, 0.5, 3],   # Particella 3, evento 1
+        ],
+        [
+            [2.0, 3.0, 4.0, 150.0, 0.6, 4],   # Particella 1, evento 2
+            [5.0, 6.0, 7.0, 250.0, 0.6, 5],   # Particella 2, evento 2
+            [8.0, 9.0, 10.0, 350.0, 0.6, 6],  # Particella 3, evento 2
+        ]
     ], dtype=torch.float32)
     
     In questo caso: batch_size = 2 (due eventi), seq_len = 3 (tre particelle per evento), feature_dim = 6 (x, y, z, E, m, ID)
-    Non riesco però a capire come fa il modello a sapere che la forma del tensore è questa
-    LEO: eh infatti.... più che altro come fa a capire che i primi tre vettori stanno insieme e i secondi tre stanno insieme....
-    questo è semplicemente l'elenco delle caratteristiche raggruppate per particella, ma poi le particelle non sono raggruppate 
-    per evento.... vero è che se tu gli dici esplicitamente che il tensore è supponiamo (400, 3, 6) allora ok, ma questo vorrebbe dire
-    che le 23 di ogni evento sono 3 (è il nostro caso?)
 '''
 
 '''
@@ -39,9 +37,10 @@ altre funzioni da fare:
 altro?
 '''
 
-class Particle_Transformer(nn.Module): # va scritto con la maiuscola, o almeno, io ho trovato solo questo. poi se volevi module minuscolo ce lo dici te
+class Particle_Transformer(nn.Module):
     '''
-        nn.module è una classe di torch per tutti i tuti di reti neurali, contiene delle subclassi come la classe transformer
+        commentino della classe
+        nn.module è una classe di torch per tutti i tipi di reti neurali, contiene delle subclassi come la classe transformer
         
         ma nn.module sarebbe un args della classe ??
     '''
@@ -57,7 +56,7 @@ class Particle_Transformer(nn.Module): # va scritto con la maiuscola, o almeno, 
         '''
         Args: 
             dim_features (int): number of features of the input (px, py, pz, E, M, ID, .. altro?)
-            N_head (int): number of heads used for the attention mechanism
+            N_head (int): number of heads used for the attention mechanism (min 6)
             N_encoder_layers (int): number of encoder layers
             N_decoder_layers (int): number of decoder layers
             N_units (int): number of units of each hidden layer
