9df43da50cea137f8e5425ff3c2673c459232170
leobm <leonardo.flag@gmail.com>
2025-04-13 15:23:31 +0200

    elimination in the imports of random_split

--- pythiatransformer/data_processing.py
+++ pythiatransformer/data_processing.py
@@ -11,7 +11,6 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import torch
 from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import random_split
 import uproot
 
 
