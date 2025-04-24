4f35a13729524ef2ba17458eb00db392e9d50307
AlbertoMontanelli <albertomontanelli@live.it>
2025-04-25 00:35:09 +0200

    minor corrections

--- pythiatransformer/main.py
+++ pythiatransformer/main.py
@@ -63,7 +63,7 @@ logger.info(
 )
 
 ##################################################################################################
-
+print("prova generazione particelle con generate_target")
 # Lunghezza massima della sequenza target da generare
 max_len = 1217  # Lunghezza delle particelle finali
 
@@ -74,7 +74,7 @@ output = transformer.generate_target(
     max_len=max_len  # Lunghezza massima da generare
 )
 
-print(f"Output shape: {output.shape}")  # Controlla la forma dell'output
+print(f"Output shape generate_target: {output.shape}")  # Controlla la forma dell'output
 
 
 ##################################################################################################
