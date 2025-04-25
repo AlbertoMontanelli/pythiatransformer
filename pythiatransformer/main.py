740f5f69acafb13e89d3964e59e0a826dbc7acf0
AlbertoMontanelli <a.montanelli@studenti.unipi.it>
2025-04-25 16:01:46 +0000

    testing the forward method to generate final particles from status 23 particles

--- pythiatransformer/main.py
+++ pythiatransformer/main.py
@@ -63,18 +63,29 @@ logger.info(
 )
 
 ##################################################################################################
-logger.info("prova generazione particelle con forward")
-output_tensor=[]
-forward_dataset = transformer.data_processing(validation_set_23, validation_set_final, shuffle=False)
-forward_mask = transformer.data_processing(attention_val_23, attention_val_final, shuffle=False)
-for (inputs, targets), (inputs_mask, targets_mask) in zip(forward_dataset, forward_mask):
-    outputs = transformer.forward(inputs, targets, inputs_mask, targets_mask)
-    #output_tensor.append(outputs)
+import h5py
+import numpy as np
 
-logger.info("fine generazione particelle con generate_target")
-logger.info(f"Output shape generate_target: {output_tensor.shape}")
+# File unico HDF5
+output_file = "output_tensor.h5"
+
+# Crea il file HDF5
+with h5py.File(output_file, "w") as h5f:
+    logger.info("Prova generazione particelle con forward")
+    forward_dataset = transformer.data_processing(validation_set_23, validation_set_final, shuffle=False)
+    forward_mask = transformer.data_processing(attention_val_23, attention_val_final, shuffle=False)
+
+    for batch_idx, ((inputs, targets), (inputs_mask, targets_mask)) in enumerate(zip(forward_dataset, forward_mask)):
+        outputs = transformer.forward(inputs, targets, inputs_mask, targets_mask)
+        outputs_np = outputs.detach().numpy()  # Converti il tensore PyTorch in array NumPy
+
+        # Crea un dataset per ogni batch
+        h5f.create_dataset(f"batch_{batch_idx}", data=outputs_np, compression="gzip")
+        logger.info(f"Batch {batch_idx+1} salvato in HDF5.")
+
+logger.info(f"Tutti i batch salvati in un unico file HDF5: {output_file}")
 ##################################################################################################
-
+"""
 train_loss, val_loss = transformer.train_val(
     num_epochs = epochs,
     loss_func = loss_func,
@@ -82,7 +93,7 @@ train_loss, val_loss = transformer.train_val(
 )
 
 plot_losses(train_loss, val_loss)
-
+"""
 # girare fastjet sull'output
 # cluster sequence
 # one hot encoding ID
