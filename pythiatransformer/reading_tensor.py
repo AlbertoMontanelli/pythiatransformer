3788ba2b52dd1dc3480705f6985be180e500d804
AlbertoMontanelli <a.montanelli@studenti.unipi.it>
2025-04-27 11:53:12 +0000

    updating: added logging print

--- pythiatransformer/reading_tensor.py
+++ pythiatransformer/reading_tensor.py
@@ -2,6 +2,8 @@ import h5py
 import torch
 from loguru import logger
 
+from data_processing import training_set_final
+
 # Carica il file HDF5
 output_file = "output_tensor.h5"
 
@@ -12,4 +14,11 @@ with h5py.File(output_file, "r") as h5f:
     # Concatena i batch (se necessario)
     final_tensor = torch.cat(saved_tensors, dim=0)
     logger.info(f"Tensore concatenato, dimensione finale: {final_tensor.shape}")
+
+for event in range (0, 50, 1):
+	print(f"particella 1 vera: {training_set_final[event, 0, :]}")
+	print(f"particella 1 ricostruita: {final_tensor[event, 0, :]}")
+	print(f"ultima particella vera: {training_set_final[event, -1, :]}")
+	print(f"ultima particella ricostruita: {final_tensor[event, -1, :]}")
+
 
