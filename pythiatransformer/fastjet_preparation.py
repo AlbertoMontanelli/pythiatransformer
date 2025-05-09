132d2c4d5643f728e6adc64ee4bce62f40afb813
leobm <leonardo.flag@gmail.com>
2025-05-09 15:31:24 +0200

    beginning of de-processing

---
+++ pythiatransformer/fastjet_preparation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Masses array; the index corresponds to the one-hot-encoding index.
+masses = np.array([
+    0.93827, 0.93957, 0.49368, 0.13957, 0, 0, 0.10566, 0, 0.000511, 4.183, 1.273, 0.0935, 0.00216, 0.0047,
+    0.0047, 0.00216, 0.0935, 1.273, 4.183, 0.000511, 0, 0.10566, 0, 0, 0, 0, 0.49761, 0.13957, 0.49368, 0.93957, 0.93827
+    ])
+
+# Associate the masses to the tensors.
+
