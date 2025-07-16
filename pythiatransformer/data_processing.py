8dc6d33c03f4c4305bf0684ad683fa4044c3eb16
leobm <147031896+leonardobandiera@users.noreply.github.com>
2025-07-17 00:03:15 +0200

    correction in one exception

--- pythiatransformer/data_processing.py
+++ pythiatransformer/data_processing.py
@@ -12,7 +12,6 @@ v. the saving of the tensors and the loaders.
 """
 
 import awkward as ak
-import math
 import numpy as np
 import torch
 import uproot
@@ -248,7 +247,7 @@ def train_val_test_split(
             "Parameter 'tensor' must be of type torch.Tensor, "
             f"got '{type(tensor)}' instead."
         )
-    if not math.isclose(train_perc + val_perc + test_perc == 1, rel_tol=1e-6):
+    if not (train_perc + val_perc + test_perc == 1):
         raise ValueError(
             "Invalid values for split percentages. Must sum to 1."
         )
