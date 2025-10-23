88f832107539bc4e3e63928c159910cf2347da14
AlbertoMontanelli <a.montanelli@studenti.unipi.it>
2025-10-23 12:07:28 +0200

    minor cleanup

--- pythiatransformer/main.py
+++ pythiatransformer/main.py
@@ -11,19 +11,6 @@ The main steps are:
 - train and validate the model;
 - plot training and validation loss curves;
 - save the trained model.
-
-Notes
------
-All input, output, and plot filenames follow a default naming
-convention unique for all ``pythiatransformer`` package.
-Each file name includes a unique ``_{suffix}`` tag identifying the
-specific dataset/plot/filename depending on the number of events.
-This suffix is the only user-controlled identifier, and it must remain
-consistent across all stages of the workflow (data generation,
-preprocessing, training, inference, and plotting).
-File names themselves should **not be changed manually**, as they are
-managed automatically by the pipeline and ensure consistent data
-linkage.
 """
 
 import argparse
